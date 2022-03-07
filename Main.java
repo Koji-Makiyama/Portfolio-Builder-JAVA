36bc332fab2a231c24d04c3da630e6c7825e6257
Alexander Koji Makiyama <98917941+Koji-Makiyama@users.noreply.github.com>
2022-03-07 09:41:36 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 public class Main {      
     public static void main(String[] args) {
         ProfileBuilder profileid1 = new ProfileBuilder();
-
         profileid1.warning();
-
         profileid1.get_fName();
         profileid1.get_mName();
         profileid1.get_lName();
@@ -11,12 +9,8 @@ public class Main {
         profileid1.get_occupation();
         profileid1.get_gender();
         profileid1.get_preferred_pronoun(); 
-
         System.out.println("Below is your compiled profile:");
         System.out.println(profileid1);
-
         profileid1.ask_question(profileid1);
-
     }
-
 }
