740e3e3db759902d4ddf0749f49ab5578bb8fed4
Alexander Koji Makiyama <98917941+Koji-Makiyama@users.noreply.github.com>
2022-03-07 10:01:13 -0700

    Update ProfileBuilder.java

--- ProfileBuilder.java
+++ ProfileBuilder.java
@@ -47,7 +47,7 @@ class ProfileBuilder implements Profile, Serializable {
                 serializeobj.writeObject(o);
                 serializeobj.close();
                 serialize.close();
-                System.out.println("On Windows, your exported ser file should be in your C:\User\(yourUsernamehere), folder";
+                System.out.println("On Windows, your exported ser file should be in your C:\\User\\(yourUsernamehere), folder. Goodbye!");
             } catch (IOException i) {
                 System.out.println(i);
             }
